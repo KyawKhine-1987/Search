02d4189fb0a12b9d093fce1ecf4cda8ca418908c
KyawKhine <kknapster99@gmail.com>
2018-08-29 11:26:31 +0630

    changed on Aug 29, 2018

--- app/src/main/java/com/freelance/android/searchinexpandablelistview/activities/MainActivity.java
+++ app/src/main/java/com/freelance/android/searchinexpandablelistview/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.freelance.android.searchinexpandablelistview;
+package com.freelance.android.searchinexpandablelistview.activities;
 
 import android.app.SearchManager;
 import android.content.Context;
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ExpandableListView;
 
+import com.freelance.android.searchinexpandablelistview.R;
 import com.freelance.android.searchinexpandablelistview.adapter.ExpandableListAdapter;
 import com.freelance.android.searchinexpandablelistview.models.ChildRow;
 import com.freelance.android.searchinexpandablelistview.models.ParentRow;
